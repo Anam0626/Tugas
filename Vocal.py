9a9162dc1666168bfdab404517f828c3c0f8c9d2
Anam0626 <82551149+Anam0626@users.noreply.github.com>
2022-09-23 09:33:05 +0700

    Update Vocal.py

--- Vocal.py
+++ Vocal.py
@@ -1,13 +1,10 @@
-from dataclasses import replace
-
-
 x = (input(""))
 
-vocal = ["a","i","u","e","o"]
+vocal = ["a","i","u","e","o","A","I","U","E","O"]
 result = ""
 
-for i in x.lower():
+for i in x:
     if i not in vocal:
-        result = result + i
+        result += i
 
 print("Result : ", result)
