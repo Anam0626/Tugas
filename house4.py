439061ffdaae565376e751315ae3d4cf319ad88e
Anam0626 <82551149+Anam0626@users.noreply.github.com>
2022-09-26 00:39:20 +0700

    Delete house4.py

--- house4.py
+++
@@ -1,9 +0,0 @@
-x = input("Name? ").lower()
-
-match x:
-    case "harry" | "hermione" | "ron":
-        print("Gryffindor")
-    case "draco":
-        print("Slytherin")
-    case _:
-        print("Who?")
